fd7fe893fa4bb3f3de8432310024a02f891f5818
Ashikur Rahman <ashikurrahman020995@gmail.com>
2023-05-04 22:27:37 +0600

    Enable line numbers, fold method, global yanking

--- lua/chadshik/init.lua
+++ lua/chadshik/init.lua
@@ -3,3 +3,7 @@ require "plugins"
 
 vim.opt.termguicolors = true
 vim.cmd.colorscheme "catppuccin-macchiato"
+vim.opt.number = true
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevel = 99
+vim.opt.clipboard = "unnamedplus"
