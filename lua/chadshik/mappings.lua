d3cb314ebace566f937ad32c365c0b48da899048
Ashikur Rahman <ashikurrahman020995@gmail.com>
2023-05-11 00:34:32 +0600

    Add key mappings for git

--- lua/chadshik/mappings.lua
+++ lua/chadshik/mappings.lua
@@ -31,3 +31,8 @@ vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { noremap = true, silent = true })
+
+-- Git
+vim.keymap.set("n", "<leader>gb", [[:Gitsigns blame_line<CR>]])
+vim.keymap.set("n", "<leader>gtb", [[:Gitsigns toggle_current_line_blame<CR>]])
+vim.keymap.set("n", "<leader>gd", [[:Gitsigns diffthis<CR>]])
