1346c8f6924e2e5fffd65129e753aeb282f34a41
Ashikur Rahman <ashikurrahman020995@gmail.com>
2023-05-13 01:26:43 +0600

    Fixed copy/paste issue

--- lua/chadshik/mappings.lua
+++ lua/chadshik/mappings.lua
@@ -18,13 +18,7 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Copy/paste
-vim.keymap.set("n", "yy", '"+yy')
-vim.keymap.set("v", "y", '"+y')
-vim.keymap.set("n", "dd", '"+dd')
-vim.keymap.set("v", "d", '"+d')
-vim.keymap.set("n", "p", '"+p')
-vim.keymap.set("v", "p", '"_d"+P')
-vim.keymap.set("n", "P", '"+P')
+vim.keymap.set("v", "p", 'p:let @+=@0<CR>:let @"=@0<CR>')
 
 -- navigate panes
 vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", { noremap = true, silent = true })
