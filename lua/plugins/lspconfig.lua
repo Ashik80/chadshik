a6fabb7c5139bf21704be0038b05164e7d85bc45
Ashikur Rahman <ashikurrahman020995@gmail.com>
2023-06-14 16:17:33 +0600

    Config for searching hidden files and remove rust lsp config out-of-the-box

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,6 @@
 local lspconfig = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local servers = { "lua_ls", "html", "cssls", "tsserver", "pyright", "pylsp", "jsonls", "eslint", "rust_analyzer", "clangd" }
+local servers = { "lua_ls", "html", "cssls", "tsserver", "pyright", "pylsp", "jsonls", "eslint", "clangd" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,11 @@
 local builtin = require('telescope.builtin')
 
+function FindAllFiles()
+  builtin.find_files({hidden=true, no_ignore=true})
+end
+
 vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fa', FindAllFiles, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
